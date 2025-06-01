e35613654f2304aad58dbf4b97888031f07815a8
Andreza27 <andrezadasilva460@gmail.com>
2025-06-01 18:24:09 -0300

    atualização do codigo

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ function geradordenumero(){
     const input01 = Math.ceil(document.querySelector('.input01').value)
     const input02 = Math.floor(document.querySelector('.input02').value)
 
+
+    if (input02 > input01) {
     const resultado = Math.floor(Math.random() * (input01-input02 + 1)) + input02;
 
-    alert(resultado)
+    alert(resultado) }
+    else {
+        alert("O valor minimo tem que ser MENOS que o valor maximo")
+    }
 }
